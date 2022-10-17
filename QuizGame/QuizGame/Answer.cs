d15a05d8457408d6ac6345eff2b82ad9b8b4aa3f
@ahermansan <aherman@san.edu.pl>
2022-10-17 12:54:15 +0200

    17.10.2022

---
+++ QuizGame/QuizGame/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
