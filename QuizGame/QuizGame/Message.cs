68d5c43558d522a14507fbac1e5bf0c041df9685
andrzej-herman <aherman@san.edu.pl>
2022-11-14 13:18:06 +0100

    Add question json

---
+++ QuizGame/QuizGame/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    class Message
+    {
+        internal void ShowWelcomeScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("*************************");
+            Console.WriteLine("** WELCOME TO OUR QUIZ **");
+            Console.WriteLine("************************");
+            Console.WriteLine("Try to answer 5 questions");
+            Console.WriteLine("************************");
+            Console.WriteLine("**     GOOD LUCK !!!  **");
+            Console.WriteLine("************************");
+            Console.WriteLine();
+        }
+    }
+}
